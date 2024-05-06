ea632fa44c83b07154d65d3f22be7c158442be1b
Alice <25051987alisa@gmail.com>
2024-05-06 20:05:12 +0200

    ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,11 +6,21 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 19:19:58 by user              #+#    #+#             */
-/*   Updated: 2024/05/06 19:48:10 by user             ###   ########.fr       */
+/*   Updated: 2024/05/06 20:04:32 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/**
+ * @brief (EN) ft_lstclear clears the entire linked list, destroying
+ * all elements in it.
+ * 
+ * @param lst Pointer to the pointer to the beginning of the list
+ * @param del Pointer to the function that will be called to free
+ * the memory allocated for the contents of each element.
+ * 
+ */
 
 /**
  * @brief (RU) Функция ft_lstclear очищает весь связный список, уничтожая все
