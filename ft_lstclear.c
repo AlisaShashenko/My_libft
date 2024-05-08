e95f92e1f8c2cb6f6b0960b8f45b1675c28f80bb
Alice <25051987alisa@gmail.com>
2024-05-08 19:43:27 +0200

    ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 19:19:58 by user              #+#    #+#             */
-/*   Updated: 2024/05/06 20:04:32 by user             ###   ########.fr       */
+/*   Updated: 2024/05/08 19:42:41 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,78 @@
  * памяти, выделенной для содержимого каждого элемента.
  */
 
+/*size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i ++;
+	}
+	return (i);
+}*/
+
+/*char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	size_t	i;
+
+	dest = (char *) malloc(ft_strlen(s1) +1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i ++;
+	}
+	dest[i] = 0;
+	return (dest);
+}*/
+
+/*t_list	*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	ret = malloc(sizeof(t_list));
+	if (!ret)
+		return (0);
+	ret->content = content;
+	ret->next = 0;
+	return (ret);
+}*/
+
+/*void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_elem;
+
+	if (!lst || !new)
+		return ;
+	last_elem = ft_lstlast(*lst);
+	if (!last_elem)
+		*lst = new;
+	else
+		last_elem->next = new;
+}*/
+
+/*t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}*/
+
+/*void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst -> content);
+	free(lst);
+}*/
+
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*next;
@@ -44,3 +116,22 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	free(*lst);
 	*lst = NULL;
 }
+
+/*void	ft_del(void *content)
+{
+	free(content);
+	printf("\nfree~\n");
+}
+
+int	main(void)
+{
+	char	*str = ft_strdup("Hello");
+	char	*str1 = ft_strdup("world");
+	t_list	*lst = ft_lstnew(str);
+	ft_lstadd_back(&lst, ft_lstnew(str1));
+
+	printf("After: %s %s\n", str, str1);
+
+	ft_lstclear(&lst, ft_del);
+	return (0);
+}*/
