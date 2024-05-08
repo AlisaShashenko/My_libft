098bcc3f4cfefdda03a47331f61594cc88d74f81
Alice <25051987alisa@gmail.com>
2024-05-08 19:17:46 +0200

    ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,11 +6,12 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 19:06:07 by user              #+#    #+#             */
-/*   Updated: 2024/05/06 19:59:38 by user             ###   ########.fr       */
+/*   Updated: 2024/05/08 19:14:05 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 /**
  * @brief (EN) ft_lstdelone destroys one element in a linked list.
@@ -30,6 +31,48 @@
  * освобождения памяти, выделенной для удаляемого элемента.
  */
 
+/*size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i ++;
+	}
+	return (i);
+}*/
+
+/*char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	size_t	i;
+
+	dest = (char *) malloc(ft_strlen(s1) +1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i ++;
+	}
+	dest[i] = 0;
+	return (dest);
+}*/
+
+/*t_list	*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	ret = malloc(sizeof(t_list));
+	if (!ret)
+		return (0);
+	ret->content = content;
+	ret->next = 0;
+	return (ret);
+}*/
+
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst || !del)
@@ -37,3 +80,23 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	del(lst -> content);
 	free(lst);
 }
+
+/*void	ft_del(void *content)
+{
+	free(content);
+}
+
+int	main(void)
+{
+	char	*str = ft_strdup("Hello");
+	char	*str1 = ft_strdup("world");
+	t_list	*lst = ft_lstnew(str);
+	t_list	*lst1 = ft_lstnew(str1);
+
+	printf("After: %s %s\n", str, str1);
+
+	ft_lstdelone(lst1, ft_del);
+
+	printf("Before: %s\n %s\n", str, str1);
+	return (0);
+}*/
